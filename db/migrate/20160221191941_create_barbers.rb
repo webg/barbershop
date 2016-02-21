2c5c97053842bef9726d4f0f93ab4da28fe4c82a
webg <diyhouse@yandex.ru>
2016-02-21 22:28:39 +0300

    Add barbers migration

---
+++ db/migrate/20160221191941_create_barbers.rb
@@ -0,0 +1,15 @@
+class CreateBarbers < ActiveRecord::Migration
+  def change
+  	create_table :barbers do |t|
+  		t.text :name
+  		
+  		t.timestamp
+	end
+	Barber.create :name => 'Jessie Pinkman'
+	Barber.create :name => 'Walter White'
+	Barber.create :name => 'Gus Fring'
+	Barber.create :name => 'Ruslan Gandaloev'
+
+  end
+
+end
