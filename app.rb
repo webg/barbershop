5356e1b6ffbd23db6277dd59b98c9388d9cf7c7d
webg <diyhouse@yandex.ru>
2016-02-21 21:58:02 +0300

    Add fix app.rb

--- app.rb
+++ app.rb
@@ -4,8 +4,9 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'sinatra/activerecord'
 
-set :database, "sqlite3: barbershop.db"
-class Client < ActiveRecord ::Base 
+set :database, "sqlite3:barbershop.db"
+
+class Client < ActiveRecord::Base 
 end
 
 get '/' do
