101c9081b3d52b02483f39fc5a66cd411dae893d
webg <diyhouse@yandex.ru>
2016-02-22 12:49:06 +0300

    Barber page

--- app.rb
+++ app.rb
@@ -7,7 +7,7 @@ require 'sinatra/activerecord'
 set :database, "sqlite3:barbershop.db"
 
 class Client < ActiveRecord::Base 
-	validates :name, presence: true
+	validates :name, presence: true, length: {minimum: 3}
 	validates :phone, presence: true
 	validates :datestamp, presence: true
 	validates :color, presence: true
@@ -38,4 +38,9 @@ post '/visit' do
 		@error = @c.errors.full_messages.first
 		erb :visit
 	end
+end
+
+get '/barber/:id' do
+	@barber = Barber.find(params[:id])
+	erb :barber
 end
