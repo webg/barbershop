0cf0249daf51880da36e054bcd874fbc9634a57e
webg <diyhouse@yandex.ru>
2016-02-22 11:38:05 +0300

    col

--- app.rb
+++ app.rb
@@ -12,7 +12,22 @@ end
 class Barber < ActiveRecord::Base
 end
 
+before do
+	@barbers = Barber.all 
+end
+
 get '/' do
-	@barbers = Barber.all
 	erb :index
+end
+
+get '/visit' do
+	erb :visit
+end
+
+post '/visit' do
+
+	c = Client.new params[:client]
+	c.save
+
+    erb "<h2> Спасибо, Вы записались</h2>"
 end
