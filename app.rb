fd90299a662063edbcabe7b861aefa428e63e1da
webg <diyhouse@yandex.ru>
2016-02-22 12:19:59 +0300

    datestamp

--- app.rb
+++ app.rb
@@ -25,16 +25,17 @@ get '/' do
 end
 
 get '/visit' do
+	@c = Client.new
 	erb :visit
 end
 
 post '/visit' do
 
-	c = Client.new params[:client]
-	if c.save
+	@c = Client.new params[:client]
+	if @c.save
 		erb "<h2> Спасибо, Вы записались</h2>"
 	else
-		@error = c.errors.full_messages.first
+		@error = @c.errors.full_messages.first
 		erb :visit
 	end
 end
